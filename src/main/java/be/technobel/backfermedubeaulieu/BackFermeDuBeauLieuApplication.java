2a46065a068190aba1d989ac652f0a8aec02e5e9
Lucas Balon <lucasbalon@gmail.com>
2024-03-05 10:56:21 +0100

    Add initial project files and set up project structure
    This commit adds the necessary initial files and sets up the structure for the project. It includes the creation of the `mvnw` wrapper script and `mvnw.cmd` file for building the project. Additionally, it includes the setup for the project POM file and the creation of the main application class. The `.gitignore` file has also been added to avoid unnecessary files being tracked by Git.

---
+++ src/main/java/be/technobel/backfermedubeaulieu/BackFermeDuBeauLieuApplication.java
@@ -0,0 +1,13 @@
+package be.technobel.backfermedubeaulieu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFermeDuBeauLieuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackFermeDuBeauLieuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/technobel/backfermedubeaulieu/BackFermeDuBeauLieuApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technobel.backfermedubeaulieu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFermeDuBeauLieuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
